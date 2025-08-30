23a885c00c021632496aacddb35d9da55ef5c7bc
Leonardo Santos <leoss33@outlook.com.br>
2025-08-29 22:22:52 -0300

    Add main.py for asset management and implement ETF class in etf.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from kotizoj.wallet import Wallet
+from kotizoj.assets.asset import Asset
+from kotizoj.assets.stock import Stock
+
+
+def asset_creator(asset_type: str, name: str, country:str, quantity:str) -> Asset: 
+    if asset_type == "stock":
+        return Stock(name, country, quantity)
+
+
+
+if __name__ == "__main__":
+    print("Hello, World!")
+    first_wallet = Wallet()
+    petr3 = asset_creator("stock","PETR3", "Brasil", 10)
+    petr4 = asset_creator("stock","PETR4", "Brasil", 10)
+    first_wallet.add_asset(petr3)
+    first_wallet.add_asset(petr4)
+    print(first_wallet)
