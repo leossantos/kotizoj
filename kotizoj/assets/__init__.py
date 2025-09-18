49c5418dbc10457753a5883cf2668a7e2dfef61c
Leonardo Santos <leoss33@outlook.com.br>
2025-09-17 21:16:06 -0300

    chore: :hammer: Update main script

---
+++ kotizoj/assets/__init__.py
@@ -0,0 +1,6 @@
+from .asset import Asset
+from .stock import Stock
+from .etf import ETF
+from .crypto_currency import CryptoCurrency
+
+__all__ = ["Asset", "Stock", "ETF", "CryptoCurrency"]
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from kotizoj.wallet import Wallet
-from kotizoj.assets.asset import Asset
-from kotizoj.assets.stock import Stock
-from kotizoj.assets.crypto_currency import CryptoCurrency
+from kotizoj.assets import Asset, Stock, CryptoCurrency, ETF
 
 
 def asset_creator(asset_type: str, name: str, country:str, quantity:str) -> Asset: 
@@ -9,16 +7,30 @@ def asset_creator(asset_type: str, name: str, country:str, quantity:str) -> Asse
         return Stock(name, country, quantity)
     if asset_type == "crypto":
         return CryptoCurrency(name, country, quantity)
+    if asset_type == "etf":
+        return ETF(name, country, quantity)
 
 
 
 if __name__ == "__main__":
     print("Hello, World!")
     first_wallet = Wallet()
-    petr3 = asset_creator("stock","PETR3", "Brasil", 10)
-    petr4 = asset_creator("stock","PETR4", "Brasil", 10)
-    bitcoin = asset_creator("crypto", "Bitcoin", "Brasil", 1)
-    first_wallet.add_asset(petr3)
-    first_wallet.add_asset(petr4)
+    agro3 = asset_creator("stock","AGRO3", "Brasil", 8)
+    b5p211 = asset_creator("etf","B5P211", "Brasil", 10)
+    bbas3 = asset_creator("stock","BBAS3", "Brasil", 8)
+    blau3 = asset_creator("stock","BLAU3", "Brasil", 20)
+    bovv11 = asset_creator("etf","BOVV11", "Brasil", 7)
+    egie3 = asset_creator("stock","EIGE3", "Brasil", 5)
+    ivvb11 = asset_creator("etf","IVVB11", "Brasil", 5)
+    wrld11 = asset_creator("etf","WRTL11", "Brasil", 14)
+    bitcoin = asset_creator("crypto", "Bitcoin", "Brasil", 0.00090177)
+    first_wallet.add_asset(agro3)
+    first_wallet.add_asset(b5p211)
+    first_wallet.add_asset(bbas3)
+    first_wallet.add_asset(blau3)
+    first_wallet.add_asset(bovv11)
+    first_wallet.add_asset(egie3)
+    first_wallet.add_asset(ivvb11)
+    first_wallet.add_asset(wrld11)
     first_wallet.add_asset(bitcoin)
     print(first_wallet)
